04079c4d5197925631aad01a2eeeb96ecf696e06
zarri2 <168563126+zarri2@users.noreply.github.com>
2024-05-30 18:39:41 +0500

    Props Challenges First challenge start

---
+++ PropsChallenges/src/App.jsx
@@ -0,0 +1,14 @@
+import ChallengeOne from './assets/Components/Challenge1Q1'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <ChallengeOne/>
+    </>
+  )
+}
+
+export default App
---
+++ PropsChallenges/src/assets/Components/Challenge1Q1.jsx
@@ -0,0 +1,11 @@
+function ChallengeOne () {
+
+    return (
+        <>
+        <h1 className="heading1">Challenge One to use a Prop method which helps Avoid duplication</h1>
+        
+        
+        </>
+    )
+}
+export default ChallengeOne;
